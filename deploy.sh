a2a15e08cf213d2e9afa98a0e8ad53b8004aa183
peppingdore <butichex@gmail.com>
2025-08-17 17:54:26 +0300

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+cd /root/vostokved/
+cd react-app
+npm run build
+cd ..
+cd images
+../minify_images.sh
+cd ..
+echo "minified"
+sudo cp -r images /var/www/react-app/images
+echo "copied images"
+sudo cp -r react-app/dist/* /var/www/react-app/
+echo "copied build"
+echo "Done!"
